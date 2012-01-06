ecc7046d2ba70d495937e8d53745931597544f5a
Darren Roberts <darren.roberts@suite101.com>
2012-01-06 11:53:28 -0800

    finished first draft of the User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120105195635) do
+ActiveRecord::Schema.define(:version => 20120106184316) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
